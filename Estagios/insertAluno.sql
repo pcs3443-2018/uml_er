baa37db6ae7f734f8927a2f70396bc0462b49969
adm <michelet@usp.br>
2018-05-08 17:59:10 -0300

    versão 1

---
+++ Estagios/insertAluno.sql
@@ -0,0 +1,5 @@
+insert into alunos (nome,numero_usp) values ('Michelet','5730682');
+insert into alunos (nome,numero_usp) values ('Pedro','5730681');
+insert into alunos (nome,numero_usp) values ('João','5730684');
+insert into alunos (nome,numero_usp) values ('José','5730683');
+insert into alunos (nome,numero_usp) values ('Maria','5730685');
---
+++ Estagios/insertEstagio.sql
@@ -0,0 +1,3 @@
+insert into estagios(data_inicio,data_fim,id_aluno,nome_empresa) values ('2018-04-01 08:00:00','2018-04-30 08:00:00',1,"Labprog");
+insert into estagios(data_inicio,data_fim,id_aluno,nome_empresa) values ('2018-01-01 08:00:00','2018-06-30 08:00:00',2,"Banco Itau");
+insert into estagios(data_inicio,data_fim,id_aluno,nome_empresa) values ('2018-01-01 08:00:00','2018-06-30 08:00:00',3,"Banco Itau");
